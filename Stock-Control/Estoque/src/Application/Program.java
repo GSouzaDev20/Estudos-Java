4dfe002ac8ea17c1a3084c8bfcd2b3f32421db09
Guilherme Fraga <119956812+GSouzaDev20@users.noreply.github.com>
2025-11-06 20:16:14 -0300

    Stock control program
    A program that make a stock using OOP for the products

---
+++ Stock-Control/Estoque/src/Application/Program.java
@@ -0,0 +1,40 @@
+package Application;
+
+import Entities.Product;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Enter Product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addQuantity(quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeQuantity(quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+    }
+}
